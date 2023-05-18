009f2fe63088146302139a60a23afbae4a101525
nims99 <128725858+nims99@users.noreply.github.com>
2023-05-18 17:22:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const PLAYER_X_CLASS = 'x';
+const PLAYER_O_CLASS = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]');
+const boardElement = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.getElementById('winningMessageText');
+let isPlayerOTurn = false;
+
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+  isPlayerOTurn = false;
+  cellElements.forEach(cell => {
+    cell.classList.remove(PLAYER_X_CLASS);
+    cell.classList.remove(PLAYER_O_CLASS);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = isPlayerOTurn ? PLAYER_O_CLASS : PLAYER_X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  isPlayerOTurn = !isPlayerOTurn;
+}
+
+function setBoardHoverClass() {
+  boardElement.classList.remove(PLAYER_X_CLASS);
+  boardElement.classList.remove(PLAYER_O_CLASS);
+  if (isPlayerOTurn) {
+    boardElement.classList.add(PLAYER_O_CLASS);
+  } else {
+    boardElement.classList.add(PLAYER_X_CLASS);
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return (
+      cell.classList.contains(PLAYER_X_CLASS) ||
+      cell.classList.contains(PLAYER_O_CLASS)
+    );
+  });
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "It's a draw!";
+  } else {
+    winningMessageTextElement.innerText = `Player ${
+      isPlayerOTurn ? "O" : "X"
+    } wins!`;
+  }
+  winningMessageElement.classList.add('show');
+}
